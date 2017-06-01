846867f7721abda4f9032aa7454daa6f071fa44f
Luan Andryl <luan.andryl@gmail.com>
2017-06-01 05:42:10 -0300

    comments lazy commit

--- 2D_interface.c
+++ 2D_interface.c
@@ -1,3 +1,4 @@
+// EUC_2D data structure
 typedef struct euc_2d {
   
   int city;
@@ -7,6 +8,7 @@ typedef struct euc_2d {
 
 }EUC_2D;
 
+// Queue data structure for EUC_2D type;
 typedef struct queue_2d{
 	
 	EUC_2D *start;
@@ -14,10 +16,12 @@ typedef struct queue_2d{
 
 }Queue_2D;
 
+// create and return a pointer type Queue_2D
 Queue_2D * create_queue () {
   return (Queue_2D *)malloc(sizeof(Queue_2D));
 }
 
+// Insert end queue function
 void insert_2D_queue(Queue_2D *q, int city, float x, float y){
 
 	EUC_2D *newBlock;
@@ -38,6 +42,7 @@ void insert_2D_queue(Queue_2D *q, int city, float x, float y){
 	}
 }
 
+// impress queue
 void impress_2d_queue(Queue_2D *q){
 
 	EUC_2D *aux = q->start;
@@ -49,23 +54,33 @@ void impress_2d_queue(Queue_2D *q){
 
 }
 
-void aloc_2D (FILE *tsblib_file) {
-  char str[128];
-  char city[2], x[55], y[55];
-
-  do {
-    if(!strcmp(str, "NODE_COORD_SECTION")) {
-      printf("Start to Aloc 2D Queue ... [%s] \n\n", str);
-      break;
+/*
+  alloc_2D 
+  Receive a pointer to file already open and put the he on 
+  right position(after NODE_COORD_SECTION) to read the data of filestream 
+*/
+void alloc_2D (FILE *tsblib_file) {
+  char str[128], city[2], x[55], y[55]; //strings that fscanf will copy the info of filestram 
+  /*
+    do while => because FILE *tsblib_file recived can be right in the next line, so
+   in this case we find first NODE_COORD_SECTION and break the loop
+  */
+  do {  
+    if(!strcmp(str, "NODE_COORD_SECTION")) { // compare
+      printf("Start to Alloc 2D Queue ... [%s] \n\n", str);
+      break; // exit loop;
     }
   } while (fscanf(tsblib_file, "%s", str) != EOF);
 
-  Queue_2D *q = create_queue ();
+  Queue_2D *q = create_queue(); // create new queue
 
+  /*
+    read and create a queue with filestream data;
+  */
   while (fscanf(tsblib_file, "%s %s %s", city, x, y) != EOF) 
-    insert_2D_queue (q, atoi(city), atof(x), atof(y));
+    insert_2D_queue (q, atoi(city), atof(x), atof(y)); // atof beacuse some data info are real numbers
   
-  impress_2d_queue(q);
-  printf("2D Queue Alocated -> Find path through Cities ... \n\n");
+  impress_2d_queue(q); // impress queue; 
+  printf("2D Queue Allocated -> Finding better path through Cities ... \n\n");
 
 }
--- init.c
+++ init.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// 2d_interface
 #include "2D_interface.c"
 
 
@@ -10,7 +10,7 @@ int main (int argc, char *argv[]) {
   // pointer to file
   FILE *open_file = fopen(argv[1], argv[2]);
   char str[128];
-  
+
   //error handle with open file
   if (open_file == NULL) {
     fprintf(stderr, "Can't open input file [%s]\n",argv[1]);
@@ -19,17 +19,17 @@ int main (int argc, char *argv[]) {
 
   printf("Read file: [%s] \n", argv[1]);
 
+  // Walking through file header to discover what interface to be called
   while (fscanf(open_file, "%s", str) != EOF) {
-    if(!strcmp(str, "EUC_2D")) {
 
-      aloc_2D(open_file);
-      
+    if(!strcmp(str, "EUC_2D")) { 
+      alloc_2D(open_file); // pass the file pointer to 2D_interface
     } else if (!strcmp(str, "EUC_3D")) {
-      break;
+      break; // ->todo
     } 
   }
 
-  fclose(open_file);
+  fclose(open_file); // close file
   
   return 0;
 }
