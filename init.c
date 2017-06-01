9f37055217772331f43bc479979f964393ebbb9d
Luan Andryl <luan.andryl@gmail.com>
2017-06-01 04:49:37 -0300

    init.c file

--- init.c
+++ init.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main () {
+#include "2D_interface.c"
+
+
+int main (int argc, char *argv[]) {
+  
+  // pointer to file
+  FILE *open_file = fopen(argv[1], argv[2]);
+  char str[128];
+  
+  //error handle with open file
+  if (open_file == NULL) {
+    fprintf(stderr, "Can't open input file [%s]\n",argv[1]);
+    exit(1);
+  }
+
+  printf("Read file: [%s] \n", argv[1]);
+
+  while (fscanf(open_file, "%s", str) != EOF) {
+    if(!strcmp(str, "EUC_2D")) {
+
+      aloc_2D(open_file);
+      
+    } else if (!strcmp(str, "EUC_3D")) {
+      break;
+    } 
+  }
+
+  fclose(open_file);
+  
   return 0;
 }
